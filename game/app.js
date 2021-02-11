42149643c90b6a29e43c21402594ad1ac3a0cbec
RheaSudesh <vrhea304@gmail.com>
2021-02-11 12:26:01 +0530

    completed purple fruit

--- game/app.js
+++ game/app.js
@@ -1,6 +1,6 @@
-
 if(localStorage.getItem("players") === null) 
-localStorage.setItem("players", JSON.stringify([]));
+  localStorage.setItem("players", JSON.stringify([]));
+
 var mainInterval = null;
 var game = {
   board:document.getElementById("board"),
@@ -12,11 +12,14 @@ var game = {
   interval:null,
   speed:200,
   busy:false,
-  point:-1,
+  minusBusy:false,
+  possibleMinusPointCoords: {row:null,ind:null},
+  point:0,
   gameOverInterval:null,
   playerName:'',
   posiblePointCoords: {row:null,ind:null},
   playerMaxScore:0,
+  
   tryStartGame:function(e){
     var element = game.getid('userLogin');
     if(e.keyCode == 13 && !element.classList.contains('hiddenObject')) game.submitPleyer('playerName');
@@ -31,19 +34,23 @@ var game = {
       this.interval=null;
       this.speed=200;
       this.busy=false;
-      this.point=-1;
+      this.point=0;
       this.posiblePointCoords = {row:null,ind:null};
+      this.possibleMinusPointCoords= {row:null,ind:null};
   },
+
   checkPlayerName:function(name){
     var playerExists = false;
     var players = JSON.parse(localStorage.getItem('players'));
     players.forEach(function(player){if(player.nickName == name) playerExists = true;});
     return playerExists;
   },
+
   getPlayerMaxScore:function(){
     var player = JSON.parse(localStorage.getItem('players')).find(function(el){ if(el.nickName == game.playerName) return el; });
     return player.maxScore;
   },
+
   submitPleyer:function(inputID){
       var name = this.getid(inputID).value.trim();
       if(name.length < 1) return alert('write your name(nickname).');
@@ -67,6 +74,7 @@ var game = {
           game.startGame();
       },1400);
   },
+
   drawBoard:function(){
       for(var i = 0; i<this.boardLength;i++){
           var row = document.createElement("div");
@@ -80,6 +88,7 @@ var game = {
           this.board.appendChild(row);
         }
   },
+
   init:function(){
       this.getAll('.textInfo').forEach(el=>el.classList.add('hiddenObject'));
       this.drawBoard();
@@ -92,18 +101,23 @@ var game = {
       document.getElementById('playerRecord').innerHTML = this.playerMaxScore;
       document.onkeydown = this.changeDirection;
   },
+
   startGame:function(){
       this.updateLeaderBoard();
       this.gameIsStarted = true;
       this.setCoords();
       this.setDirection('right');
       this.getNewPointBox();
+      this.getNewMinusPointBox();
   },
-  setDirection:function(dir,isManual = false){
+
+  setDirection:function(dir,isManual= false){
     this.direction = dir;
     if(!isManual) this.interval = setInterval(this.runSnakeRun,this.speed);
     else this.runSnakeRun(isManual);
   },
+
+  /********************************** FRUITS AND POINTS ***************************************************/
   getPointsCoords:function(){
     var self = this;
     var element = self.getAll('.green')[0];
@@ -113,36 +127,30 @@ var game = {
     var row = parseInt(parent.getAttribute("index"));
     return {row:row,ind:ind};
   },
-  setCoords:function(){
-    if(this.busy) return;
-    this.busy = true;
-    var self = this;
-    var pointsCoords = self.getPointsCoords();
-    var isPoint = false;
-    var snakeHead = this.snakeCoords[this.snakeCoords.length-1];
-    var snakeTail = this.snakeCoords[0];
 
-    if(pointsCoords.row == snakeHead.row && pointsCoords.ind == snakeHead.ind) {
-      isPoint = true;
-      self.removeOldPoint();
-      this.snakeCoords.unshift({row:self.posiblePointCoords.row,ind:self.posiblePointCoords.ind});
-    }
-    var lastIndex = this.snakeCoords.length-1;
-    this.snakeCoords.forEach(function(el,ind){
-      if(lastIndex == ind) self.rows[el.row].querySelectorAll('.box')[el.ind].classList.add('snakeHead');
-      self.rows[el.row].querySelectorAll('.box')[el.ind].classList.add('black');
-    });
-    if(isPoint) self.getNewPointBox();
-    this.busy = false;
-    return self;
+  getMinusPointsCoords:function(){
+    var self = this;
+    var element = self.getAll('.purple')[0];
+    if(typeof element == 'undefined') return {row:-1,ind:-1};
+    var parent = element.parentNode;
+    var ind = [].indexOf.call(parent.children, element);
+    var row = parseInt(parent.getAttribute("index"));
+    return {row:row,ind:ind};
   },
+
   removeOldPoint:function(){
     this.getAll('.green')[0].classList.remove('green');
   },
+
+  removeOldMinusPoint:function(){
+    this.getAll('.purple')[0].classList.remove('purple');
+  },
+
   getRandomIndex:function (min,max)
   {
       return Math.floor(Math.random()*(max-min+1)+min);
   },
+
   getNewPointBox:function(){
       this.point = this.point + 1;
       this.speed -= 1;
@@ -152,19 +160,78 @@ var game = {
       var boxes = document.querySelectorAll('.box');
       var randomIndex = this.getRandomIndex(0,boxes.length-1);
       var randomBox = boxes[randomIndex];
-      if(randomBox.classList.contains('black')) return this.getNewPointBox();
+      if(randomBox.classList.contains('black')) 
+        return this.getNewPointBox();
       else randomBox.classList.add('green');
   },
+
+  getNewMinusPointBox:function(){
+    this.point = this.point - 1;
+    this.speed -= 1;
+    this.updatePlayerMaxScore();
+    this.updateLeaderBoard();
+    this.getid("playerScore").innerHTML = this.point;
+    var boxes = document.querySelectorAll('.box');
+    var randomIndex = this.getRandomIndex(0,boxes.length-1);
+    var randomBox = boxes[randomIndex];
+    if(randomBox.classList.contains('black')) return this.getNewMinusPointBox();
+    else randomBox.classList.add('purple');
+  },
+
+  setCoords:function(){
+    if(this.busy) return;
+    this.busy = true;
+    var self = this;
+    var pointsCoords = self.getPointsCoords();
+    var pointsMinusCoords=self.getMinusPointsCoords();
+    var isPoint = false;
+    var isMinusPoint=false;
+    var snakeHead = this.snakeCoords[this.snakeCoords.length-1];
+    var snakeTail = this.snakeCoords[0];
+
+    if(pointsCoords.row == snakeHead.row && pointsCoords.ind == snakeHead.ind) {
+      isPoint = true;
+      self.removeOldPoint();
+      this.snakeCoords.unshift({row:self.posiblePointCoords.row,ind:self.posiblePointCoords.ind});
+    }
+
+    if(pointsMinusCoords.row == snakeHead.row && pointsMinusCoords.ind == snakeHead.ind) {
+      isMinusPoint = true;
+      self.removeOldMinusPoint();
+      this.snakeCoords.shift();
+      if(snakeHead==snakeTail)
+      {
+        setTimeout(self.setGameOverContent,500);
+      }
+    }
+
+    var lastIndex = this.snakeCoords.length-1;
+    this.snakeCoords.forEach(function(el,ind){
+      if(lastIndex == ind) self.rows[el.row].querySelectorAll('.box')[el.ind].classList.add('snakeHead');
+      self.rows[el.row].querySelectorAll('.box')[el.ind].classList.add('black');
+    });
+
+    if(isPoint) self.getNewPointBox();
+    if(isMinusPoint) self.getNewMinusPointBox();
+    this.minusBusy=false;
+    this.busy = false;
+    return self;
+  },
+
+  /************************************************************************************************************/
   getAll:function(query){
     return document.querySelectorAll(query);
   },
+
   getid:function(elem){
     return document.getElementById(elem);
   },
+
   removeOldCoords:function(){
     this.getAll('.black').forEach(el => el.classList.remove('black','snakeHead'));
     return this;
   },
+
   runSnakeRun:function(changeDir = false){
     var self = game;
     var newRow,newInd;
@@ -210,63 +277,7 @@ var game = {
     self.removeOldCoords().setCoords();
     if(changeDir) this.interval = setInterval(this.runSnakeRun,this.speed);
   },
-  savePosiblePointCoords:function(el){
-    this.posiblePointCoords.row = el.row;
-    this.posiblePointCoords.ind = el.ind;
-  },
-  checkCoords:function(arr,row,ind){
-    var gameContinues = true;
-    for(var obj of arr){
-      if(obj.row == row && obj.ind == ind) return gameContinues = false;
-    }
-    return gameContinues;
-  },
-  updatePlayerMaxScore:function(){
-    var self = this;
-    var players = JSON.parse(localStorage.getItem('players'));
 
-    players.map(function(el){
-      if(el.nickName == self.playerName){
-         self.playerMaxScore = (self.point <= self.playerMaxScore) ? self.playerMaxScore : self.point;
-         el.maxScore = self.playerMaxScore;
-      }
-      return el;
-    });
-
-    localStorage.setItem('players',JSON.stringify(players));
-
-  },
-  gameOver:function(){
-    var self = this;
-    clearInterval(this.interval);
-    this.gameIsStarted = false;
-    this.updatePlayerMaxScore();
-    this.getid('playerRecord').innerHTML = this.playerMaxScore;
-    self.gameOverInterval = setInterval(function(){
-      self.getAll('.black').forEach(function(el,ind){ el.classList.toggle('fakeBlack'); });
-    },200);
-
-    setTimeout(self.setGameOverContent,2000);
-  },
-  setGameOverContent:function(){
-    var self = game;
-    clearInterval(self.gameOverInterval);
-    self.board.style.width = self.board.offsetWidth + "px";
-    self.board.style.height = self.board.offsetHeight + "px";
-    self.board.innerHTML = self.getid('gameOverContent').innerHTML;
-    self.resetGameParameters();
-    self.updateLeaderBoard();
-  },
-  changePlayer:function(){
-    var newPlayer = this.getid('playerName').value.trim();
-    if(newPlayer.length < 1) return alert('type new player name!');
-    this.playerName = newPlayer;
-  },
-  restartGame:function(){
-    this.board.innerHTML = "";
-    this.init();
-    this.startGame();
-  },
   changeDirection:function(e){
     var self = game;
     if(!self.gameIsStarted) return;
@@ -290,6 +301,76 @@ var game = {
         default: return;
       }
   },
+
+  savePosiblePointCoords:function(el){
+    this.posiblePointCoords.row = el.row;
+    this.posiblePointCoords.ind = el.ind;
+  },
+
+  checkCoords:function(arr,row,ind){
+    var gameContinues = true;
+    for(var obj of arr){
+      if(obj.row == row && obj.ind == ind) return gameContinues = false;
+    }
+    return gameContinues;
+  },
+
+
+
+  /************************************************** Game over ********************************************************/
+  updatePlayerMaxScore:function(){
+    var self = this;
+    var players = JSON.parse(localStorage.getItem('players'));
+
+    players.map(function(el){
+      if(el.nickName == self.playerName){
+         self.playerMaxScore = (self.point <= self.playerMaxScore) ? self.playerMaxScore : self.point;
+         el.maxScore = self.playerMaxScore;
+      }
+      return el;
+    });
+
+    localStorage.setItem('players',JSON.stringify(players));
+
+  },
+
+  gameOver:function(){
+    var self = this;
+    clearInterval(this.interval);
+    this.gameIsStarted = false;
+    this.updatePlayerMaxScore();
+    this.getid('playerRecord').innerHTML = this.playerMaxScore;
+    self.gameOverInterval = setInterval(function(){
+      self.getAll('.black').forEach(function(el,ind){ el.classList.toggle('fakeBlack'); });
+    },200);
+
+    setTimeout(self.setGameOverContent,2000);
+  },
+
+  setGameOverContent:function(){
+    var self = game;
+    clearInterval(self.gameOverInterval);
+    self.board.style.width = self.board.offsetWidth + "px";
+    self.board.style.height = self.board.offsetHeight + "px";
+    self.board.innerHTML = self.getid('gameOverContent').innerHTML;
+    self.resetGameParameters();
+    self.updateLeaderBoard();
+  },
+
+  changePlayer:function(){
+    var newPlayer = this.getid('playerName').value.trim();
+    if(newPlayer.length < 1) return alert('type new player name!');
+    this.playerName = newPlayer;
+  },
+
+  restartGame:function(){
+    this.board.innerHTML = "";
+    this.init();
+    this.startGame();
+  },
+
+  /**********************************************************************************************************************/
+  
   updateLeaderBoard:function(){
     var self = this;
     var tbody = self.getid('tbody');
